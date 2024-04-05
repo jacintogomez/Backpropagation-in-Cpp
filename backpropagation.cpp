c452ed95750a329585b16ed61b0819ba5793131e
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-04-05 00:58:39 -0400

    forward and backward pass

--- backpropagation.cpp
+++ backpropagation.cpp
@@ -22,11 +22,15 @@ double sigmoid(double z){
 class Neuron{
 public:
     Neuron(int i):inputs(i){}
+    Neuron(vd wts,double nw,double bi):weights(wts),neuron_weight(nw),bias(bi){
+        cout<<"made neuron with weights: "<<wts[0]<<endl;
+    }
     int inputs;
     double bias,z=0,result;
     double neuron_weight;
     vd weights;
     void activate(vd input){
+        z=0;
         for(int x=0;x<input.size();x++){
             z+=weights[x]*input[x];
         }
@@ -37,22 +41,28 @@ public:
 
 class Network{
 public:
-    Network(int i,int n,int e):inputs(i),num_neurons(n),epochs(e){
+    Network(int i,int n):inputs(i),num_neurons(n){
         for(int x=0;x<num_neurons;x++){
             Neuron nk(inputs);
             neurons.push_back(nk);
         }
     }
+    Network(vector<vd> wts,vd nw,vd bi,int i,int n):inputs(i),num_neurons(n){
+        for(int x=0;x<num_neurons;x++){
+            Neuron nk(wts[x],nw[x],bi[x]);
+            neurons.push_back(nk);
+        }
+    }
     int inputs;
     int num_neurons;
-    int epochs;
-    int network_bias;
-    int prediction=0;
+    double network_bias=0.1;
+    double prediction=0;
     double eta=0.01; //learning rate
     vd input;
     double actual;
     vector<Neuron> neurons;
     void forwardpropagation(){
+        prediction=0;
         for(Neuron &n:neurons){
             n.activate(input);
             prediction+=n.result*n.neuron_weight;
@@ -65,15 +75,19 @@ public:
         network_bias-=eta*dcost_dpred; //update final bias
         for(Neuron &n:neurons){
             n.result-=eta*dcost_dpred;
-            n.bias-=eta*dcost_dpred;
+            n.neuron_weight-=eta*dcost_dpred;
             double z=n.z;
             double dpred_dg=sigmoid(z)*(1-sigmoid(z));
             double dcost_intermediate=dcost_dpred*n.neuron_weight*dpred_dg;
-            n.neuron_weight-=eta*dcost_intermediate;
+            n.bias-=eta*dcost_intermediate;
             for(int x=0;x<inputs;x++){
                 n.weights[x]-=eta*dcost_intermediate*input[x];
+                cout<<"w: "<<n.weights[x]<<endl;
             }
+            cout<<"W: "<<n.neuron_weight<<endl;
+            cout<<"b: "<<n.bias<<endl;
         }
+        cout<<"B: "<<network_bias<<endl;
     }
 };
 
@@ -103,13 +117,13 @@ vector<Student> read_in_data(){
     vector<Student> studentdata;
     while(infile>>h1){
         //vd params={h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,e1,e2};
-        vd params={h1,h2,h3};
-        for(int i=1;i<params.size();i++){
-            infile.ignore(999,'\t');
-            infile>>params[i];
-        }
-        infile.ignore(999,'\t');
+        infile.ignore(999,' ');
+        infile>>h2;
+        infile.ignore(999,' ');
+        infile>>h3;
+        infile.ignore(999,' ');
         infile>>f;
+        vd params={h1,h2,h3};
         Student s(params,f);
         studentdata.push_back(s);
     }
@@ -124,8 +138,17 @@ vector<Student> read_in_data(){
 }
 
 int main(){
+    vector<vd> nest={{0.1,0.15,0.1},{0.05,0.1,0.1}};
+    vd wts={12,9};
+    vd biases={0.1,0.1};
     vector<Student> data=read_in_data();
     int numof_neurons=2,numof_inputs=3,epochs=1;
-    Network net(numof_inputs,numof_neurons,epochs);
+    Network net(nest,wts,biases,numof_inputs,numof_neurons);
+    for(int x=0;x<epochs;x++){
+        net.input=data[x].work;
+        net.actual=data[x].fin;
+        net.forwardpropagation();
+        net.backpropagation();
+    }
     return 0;
 }
